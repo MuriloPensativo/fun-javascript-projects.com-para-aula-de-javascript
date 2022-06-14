1bddb0351cff7c655457feefe38fe223a130f31d
caiovieira <95643550+caioo1@users.noreply.github.com>
2022-06-14 18:58:45 -0300

    testt

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function selecionarMusica(e) {
     document.querySelector("#disco").classList.remove("pulse");
 
     let source = document.getElementById("source");
-    source.src = "musias/" + e.target.innerText + ".mp3";
+    source.src = "musicas/" + e.target.innerText + ".mp3";
     document.getElementById("musicaAtual").innerText = `Está tocando: ${e.target.innerText}`;
 
     reprodutor.load();
